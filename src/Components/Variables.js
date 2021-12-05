fe90000bb37adcca67a23a3e6f110edceea1eff3
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-12-05 17:17:45 +0100

    test

--- src/Components/Variables.js
+++ src/Components/Variables.js
@@ -3,9 +3,9 @@ export const test = [
     theme: "Fotografia",
     comments:"0 comentarios",
     picUrl:"https://i.blogs.es/4c7b60/portadaresena-ghostbusters-afterlife_-el-apapacho-al-corazon-que-probablemente-necesitas/1366_2000.jpeg",
-
     initialContent:"hola a todos",
     fullContent:"hola a todos esto es el mejor dia de mi vida"},
+
     {title : "Holaaaaaaaaaaaaaaaa",
     theme: "Fotografia",
     comments:"0 comentarios",
@@ -13,4 +13,5 @@ export const test = [
     initialContent:"hola a todos",
     fullContent:"hola a todos esto es el mejor vida"
     }
+    //comentario de test
 ]
