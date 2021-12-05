9b0f20b1edade3d03b80de1eb7c5fe635397bd60
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-12-05 17:33:42 +0100

    test comment

--- src/Components/Variables.js
+++ src/Components/Variables.js
@@ -3,9 +3,9 @@ export const test = [
     theme: "Fotografia",
     comments:"0 comentarios",
     picUrl:"https://i.blogs.es/4c7b60/portadaresena-ghostbusters-afterlife_-el-apapacho-al-corazon-que-probablemente-necesitas/1366_2000.jpeg",
+
     initialContent:"hola a todos",
     fullContent:"hola a todos esto es el mejor dia de mi vida"},
-
     {title : "Holaaaaaaaaaaaaaaaa",
     theme: "Fotografia",
     comments:"0 comentarios",
@@ -14,3 +14,4 @@ export const test = [
     fullContent:"hola a todos esto es el mejor vida"
     }
 ]
+//comentario de prueba
