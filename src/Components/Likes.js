894e0ab5823999c0a2d447e18409d7db219fb09e
Pablo <pabloperezsada98@gmail.com>
2021-12-05 15:37:35 +0100

    improveNews

--- src/Components/Likes.js
+++ src/Components/Likes.js
@@ -5,21 +5,37 @@ class Likes extends React.Component{
     constructor(props) {
         super(props)
         this.state = {countLikes:0,countDislikes:0}
-        this.handleDislikes = this.handleDislikes.bind(this);
-        this.handleLikes = this.handleLikes.bind(this);
+        this.handleSumDislikes = this.handleSumDislikes.bind(this);
+        this.handleSumLikes = this.handleSumLikes.bind(this);
+        this.handleSubtractLikes = this.handleSubtractLikes.bind(this);
+        this.handleSumDislikes = this.handleSumDislikes.bind(this);
+        this.handleSubtractDislikes = this.handleSubtractDislikes.bind(this)
+
     }
-    handleLikes(){
+    handleSumLikes(){
         this.setState({countLikes : this.state.countLikes + 1})
     }
-    handleDislikes(){
+    handleSumDislikes(){
         this.setState({countDislikes : this.state.countDislikes +1})
+    }
+    handleSubtractLikes() {
+        if(this.state.countLikes > 0) {
+          this.setState({countLikes : this.state.countLikes -1})  
+        }
+        
+    }
+    handleSubtractDislikes() {
+        if(this.state.countDislikes > 0) {
+            this.setState({countDislikes : this.state.countDislikes -1})
+        }
+        
     }
 
     render() {
         let likes = undefined
         let dislikes = undefined
-        likes = <div className="likes"><img onClick={this.handleLikes} src="https://cdn-icons.flaticon.com/png/128/880/premium/880605.png?token=exp=1638708700~hmac=e23cb47e03c3de3e92b675ac5cbd46c2"width="20px" height="20px" alt="loading"></img> {this.state.countLikes} likes</div>
-        dislikes = <div className="dislikes"><img onClick={this.handleDislikes}  src="https://cdn-icons.flaticon.com/png/128/880/premium/880613.png?token=exp=1638707167~hmac=3e656528011b351adac93c9cd1c5524a" width="20px" height="20px" alt="loading"></img> {this.state.countDislikes} dislikes</div>
+        likes = <div className="likes"><img  src="https://cdn-icons.flaticon.com/png/128/880/premium/880605.png?token=exp=1638708700~hmac=e23cb47e03c3de3e92b675ac5cbd46c2"width="20px" height="20px" alt="loading"></img><img onClick={this.handleSumLikes} src="https://cdn-icons-png.flaticon.com/128/626/626075.png"width="20px" height="20px" alt="loading"></img><img onClick={this.handleSubtractLikes} src="https://cdn-icons-png.flaticon.com/128/626/626013.png"width="20px" height="20px" alt="loading"></img> {this.state.countLikes} likes</div>
+        dislikes = <div className="dislikes"><img src="https://cdn-icons.flaticon.com/png/128/880/premium/880613.png?token=exp=1638707167~hmac=3e656528011b351adac93c9cd1c5524a" width="20px" height="20px" alt="loading"></img><img onClick={this.handleSumDislikes} src="https://cdn-icons-png.flaticon.com/128/626/626075.png"width="20px" height="20px" alt="loading"></img><img onClick={this.handleSubtractDislikes} src="https://cdn-icons-png.flaticon.com/128/626/626013.png"width="20px" height="20px" alt="loading"></img>  {this.state.countDislikes} dislikes</div>
         
         return <div className="countOfLikes">{likes} {dislikes}</div>
     }
--- src/Components/Variables.js
+++ src/Components/Variables.js
@@ -3,7 +3,7 @@ export const test = [
     theme: "Fotografia",
     comments:"0 comentarios",
     picUrl:"https://i.blogs.es/4c7b60/portadaresena-ghostbusters-afterlife_-el-apapacho-al-corazon-que-probablemente-necesitas/1366_2000.jpeg",
-    
+
     initialContent:"hola a todos",
     fullContent:"hola a todos esto es el mejor dia de mi vida"},
     {title : "Holaaaaaaaaaaaaaaaa",
@@ -11,6 +11,6 @@ export const test = [
     comments:"0 comentarios",
     picUrl:"https://i.blogs.es/4c7b60/portadaresena-ghostbusters-afterlife_-el-apapacho-al-corazon-que-probablemente-necesitas/1366_2000.jpeg",
     initialContent:"hola a todos",
-    fullContent:"hola a todos esto es el mejor dia de mi vida"
+    fullContent:"hola a todos esto es el mejor vida"
     }
 ]
