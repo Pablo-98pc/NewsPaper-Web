e9729c8fa1ce11b21cdd5dac3ce13fcf99127dee
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-12-06 17:33:02 +0100

    add header component

--- src/Components/Variables.js
+++ src/Components/Variables.js
@@ -15,3 +15,14 @@ export const test = [
     },
 
 ]
+
+export const sections = [
+    {picLogo : "https://1757140519.rsc.cdn77.org/blog/wp-content/uploads/sites/4/2019/03/0019_t_usa-today-logo_1.png"},
+    {title : "Fotografía"},
+    {title : "Fotografía"},
+    {title : "Fotografía"},
+    {title : "Fotografía"},
+    {title : "Fotografía"},
+    {title : "Fotografía"},
+
+]
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import New from './Components/New';
+import Header from './Components/Header';
 import { test } from './Components/Variables.js';
+import { sections } from './Components/Variables.js';
 
 
 class Html extends React.Component {
@@ -12,6 +14,7 @@ class Html extends React.Component {
   }
 render (){
   return <>
+  <Header sections={sections}/>
   <New test = {test}/>
   </>
 }
